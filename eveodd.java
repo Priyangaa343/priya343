e5c4bf4f28a8ce7eed9a9107a1ee383770c37107
Priyangaa343 <priyangaa343@gmail.com>
2017-07-17 10:39:47 +0530

    Create eveodd.java

---
+++ eveodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class eveodd
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+    }
+}

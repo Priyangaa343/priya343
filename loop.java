186a7e78e94843e261ca8a4d30b2aa101daa45b3
Priyangaa343 <priyangaa343@gmail.com>
2017-07-17 11:33:21 +0530

    Create loop.java

---
+++ loop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class loop
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a="Hello";
+        int n=5;
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a);
+        }
+    }
+}

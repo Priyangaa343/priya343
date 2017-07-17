ddc3a58dd93ed4921438e630e78bade3d5c1c17d
Priyangaa343 <priyangaa343@gmail.com>
2017-07-17 11:42:14 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class loops
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a="Hello";
+        int n=5;
+        if(n=5)
+        {
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a);
+        }}
+    }
+}

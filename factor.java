1c9d0fbe7c432c4ee9d80dead35a7e778757ea1d
Priyangaa343 <priyangaa343@gmail.com>
2017-07-18 15:11:10 +0530

    Create factor.java

---
+++ factor.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class factor
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int sum=1;
+        for(int i=n;i>=1;i--)
+        {
+            sum=sum*i;
+        }
+        System.out.println(sum);
+    }
+}
